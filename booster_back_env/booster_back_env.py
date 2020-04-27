f1b16d8af7a4ae84f1c9810f64422c3cfdbe2e56
rlygr <rivesunder@protonmail.com>
2020-04-27 01:02:32 -0600

    define episode end states (crash or land)

--- booster_back_env/booster_back_env.py
+++ booster_back_env/booster_back_env.py
@@ -27,80 +27,18 @@ class BoosterBackEnv(gym.Env):
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
     def create_rocket(self, height=1.0, radius=1.):
-
-        self.bot_id = p.createCollisionShape(shapeType=p.GEOM_CYLINDER, height=height, radius=radius)
-
-
-        shift = [0.0, 0.0, 1.0 ]
-        orientation = p.getQuaternionFromEuler(\
-                [np.random.randn(1)*1e-1,np.random.randn(1)*1e-1,np.random.rand(1)*1e-1])
-        path = os.path.abspath(os.path.dirname(__file__))   
-        mesh_scale = [1.0,1.0,1.0]
-        shift_body = [0.0,0.0,1.1]
-        shift_nosecone = [0.0,0.0,height+0.1]
-        
-
-        self.body_visual_id = p.createVisualShape(shapeType=p.GEOM_CYLINDER,
-                                    radius=radius,
-                                    length=height,
-                                    rgbaColor=[0.5, 0.5, 0.65, 1.0],
-                                    specularColor=[0.8, .0, 0],
-                                    visualFramePosition=shift_body) 
-        self.body_collision_id = p.createCollisionShape(shapeType=p.GEOM_CYLINDER,
-                                radius=radius/2,
-                                height=height,
-                                collisionFramePosition=shift_body)
-        self.nozzle_visual_id= p.createVisualShape(shapeType=p.GEOM_MESH,
-                                fileName=os.path.join(path,"bell_nozzle.stl"),
-                                rgbaColor=[0.1,0.1,0.1,1.0],
-                                meshScale=mesh_scale)
-        self.nozzle_collision_id= p.createCollisionShape(shapeType=p.GEOM_MESH,
-                                fileName=os.path.join(path,"bell_nozzle.stl"),
-                                meshScale= mesh_scale)
-        self.nosecone_visual_id= p.createVisualShape(shapeType=p.GEOM_MESH,
-                                fileName=os.path.join(path,"nose_cone.stl"),
-                                rgbaColor=[0.5,0.5,0.75,1.0],
-                                visualFramePosition=shift_nosecone,
-                                meshScale=mesh_scale)
-        self.nosecone_collision_id= p.createCollisionShape(shapeType=p.GEOM_MESH,
-                                fileName=os.path.join(path,"nose_cone.stl"),
-                                collisionFramePosition=shift_nosecone,
-                                meshScale= mesh_scale)
-
-#        temp = p.createMultiBody(baseMass=10,
-#                baseInertialFramePosition=shift_body,
-#                baseInertialFrameOrientation=orientation,
-#                baseVisualShapeIndex=self.body_visual_id,
-#                baseCollisionShapeIndex=self.body_collision_id,
-#                basePosition=shift_body)
-        self.rocket_id = p.createMultiBody(baseMass=10,
-                          baseInertialFramePosition=shift,
-                          baseInertialFrameOrientation=orientation,
-                          baseVisualShapeIndex=self.nozzle_visual_id,
-                          baseCollisionShapeIndex=self.nozzle_collision_id,
-                          linkVisualShapeIndices=[self.nosecone_visual_id],
-                          linkCollisionShapeIndices=[self.nosecone_collision_id],
-                          linkMasses = [10.0],
-                          linkPositions=[[0,0,10]],
-                          linkOrientations=[orientation],
-                          linkInertialFramePositions=[[0,0,10]],
-                          linkInertialFrameOrientations=[orientation],
-                          linkJointAxis=[0],
-                          linkParentIndices=[0],
-                          linkJointTypes=[p.JOINT_REVOLUTE],
-                          basePosition=shift)  
-
-        p.changeDynamics(self.rocket_id, -1, linearDamping=0, angularDamping=0)
+        pass
 
     def reset(self):
         p.resetSimulation()
         p.setGravity(0, 0, -9.8)
         p.setTimeStep(0.01)
-        plane_ID = p.loadURDF("plane.urdf")
+        self.plane_id = p.loadURDF("plane.urdf")
+        self.dry_weight = 20.
 
         path = os.path.abspath(os.path.dirname(__file__))   
 
-        shift = [np.random.randn()*10, np.random.randn()*10, 30 + np.random.random()*20]
+        shift = [0.,0.,2.] #[np.random.randn()*10, np.random.randn()*10, .30 + np.random.random()*.20]
 
         orientation = p.getQuaternionFromEuler(\
                 [np.random.randn(1)*1e-1,np.random.randn(1)*1e-1,np.random.rand(1)*1e-1])
@@ -109,16 +47,16 @@ class BoosterBackEnv(gym.Env):
             orientation)
         #self.create_rocket()
 
-        info = p.getJointInfo(self.bot_id, 0)
 
-        print(info)
-        p.changeDynamics(self.bot_id,-1, lateralFriction=self.k_friction)
 
         self.fuel = 100.0
         self.kg_per_kN = 0.3 / 240 # g/(kN*s). 240 is due to the 240 Hz time step in the physics simulatorchangeDynamicsp.change
+        p.changeDynamics(self.bot_id,-1, mass = 3.0)
+        p.changeDynamics(self.bot_id, 2, mass = 1.0)
+        p.changeDynamics(self.bot_id, 1, mass = self.dry_weight + self.fuel)
 
         # get rid of damping
-        num_links = 3
+        num_links = 7
         for ii in range(num_links):
             p.changeDynamics(self.bot_id, ii, angularDamping=0.0, linearDamping=0.0)
         p.changeDynamics(self.bot_id, 1, mass=self.fuel)
@@ -140,7 +78,6 @@ class BoosterBackEnv(gym.Env):
 
         if self.fuel > 0.0:
             thrust = np.min([self.fuel / self.kg_per_kN, thrust])
-            print(thrust)
             # apply thrust to the axis of the rocket's bell nozzle
             p.applyExternalForce(self.bot_id, -1, [0.0, 0.0, thrust],\
                     posObj=[0,0,1.0],flags=p.LINK_FRAME)
@@ -148,7 +85,7 @@ class BoosterBackEnv(gym.Env):
             # decrement fuel proportional to the thrust used
             self.fuel -= thrust * self.kg_per_kN
 
-            p.changeDynamics(self.bot_id, 1, mass=self.fuel)
+            p.changeDynamics(self.bot_id, 1, mass=self.fuel+self.dry_weight)
             self.fuel = 0.0 if self.fuel <= 0.0 else self.fuel
 
         # visual indicator of thrust
@@ -166,7 +103,7 @@ class BoosterBackEnv(gym.Env):
             p.applyExternalForce(self.bot_id, 1, [thrust_x, thrust_y, 0.0],\
                     posObj=[0.0, 0.0, 5.5], flags=p.LINK_FRAME)
 
-            p.changeDynamics(self.bot_id, 1, mass=self.fuel)
+            p.changeDynamics(self.bot_id, 1, mass=self.fuel+self.dry_weight)
             self.fuel = 0.0 if self.fuel <= 0.0 else self.fuel
 
     def step(self, action):
@@ -179,10 +116,29 @@ class BoosterBackEnv(gym.Env):
         p.stepSimulation()
         
         velocity = p.getBaseVelocity(self.bot_id)
-
-        obs, reward, info = None, None, None
+        nosecone_state = p.getLinkState(self.bot_id, 4)
+        bell_nozzle_state = p.getLinkState(self.bot_id,0)
+        
+        obs, reward, info = None, 0.0, None
 
         done = False
+
+        # check for collision nose cone on ground plane
+        nose_contact_points= p.getContactPoints(self.bot_id, self.plane_id, 1)
+
+        if len(nose_contact_points) > 0:
+            print("nose ground collsion")
+            done = True
+            reward -= 300.0
+            # nose cone is on the ground
+        elif np.abs(np.mean(velocity[0])) < 1e-5:
+            print("bell is down")
+            done = True
+            reward += 100.0
+
+        if done:
+            reward += self.fuel
+            
         return obs, reward, done, info
 
     
@@ -193,9 +149,11 @@ if __name__ == "__main__":
     env = BoosterBackEnv(render=True)
     env.reset()
 
-    import pdb; pdb.set_trace()
-    for cc in range(1000):
-        action = [10 * np.random.randn(), 10. *np.random.randn(), 500]
-        env.step(action)
+    done = False
+    while not done:
+
+        action = [0,0,0] #[1 * np.random.randn(), 1. *np.random.randn(),5.0] 
+        obs, reward, done, info = env.step(action)
         time.sleep(0.01)
+
     import pdb; pdb.set_trace()
